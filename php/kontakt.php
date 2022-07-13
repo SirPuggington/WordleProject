b503bfe5cf7bd2b55310c55d0e283ac7e73d3950
Timothy Spengler <humanbeeing@live.de>
2022-07-13 11:18:00 +0200

    added functionality to contact form

--- php/kontakt.php
+++ php/kontakt.php
@@ -5,14 +5,18 @@
     <title>Kontakt</title>
 </head>
 <body>
-    <?php include __DIR__."/php/templates/header.php" ?>
+    <?php include __DIR__."/templates/header.php" ?>
 
-<form action="//" method="post">
+
+<?php if(!isset($_POST["submitted"])){ ?>
+
+
+<form action="<?php __DIR__."/php/kontakt.php" ?>" method="post">
 
 <label for="kontakt_name">Name</label>
-<input id="kontakt_name" type="text" placeholder="Vor- und Nachname" required>
+<input name="kontakt_name" id="kontakt_name" type="text" placeholder="Vor- und Nachname" required>
 <label for="kontakt_email">Email</label>
-<input id="kontakt_email" type="email" placeholder="email@adresse.de" required>
+<input name="kontakt_email" id="kontakt_email" type="email" placeholder="email@adresse.de" required>
 <label for="kontakt_thema">Thema</label>
 <select name="kontakt_thema" id="kontakt_thema">
     <option value="Fragen">Fragen</option>
@@ -20,14 +24,19 @@
     <option value="Sonstiges">Sonstiges</option>
 </select>
 <label for="kontakt_nachricht">Nachricht</label>
-<input id="kontakt_nachricht" type="textarea" placeholder="Was können wir für Sie tun?" required>
-<input id=newsletter type="checkbox">
+<input name="kontakt_nachricht"id="kontakt_nachricht" type="textarea" placeholder="Was können wir für Sie tun?" required>
+<input name="newsletter" id=newsletter type="checkbox">
 <label for="newsletter">Ich möchte mich für den Newsletter anmelden</label>
-<button type="submit">Absenden</button>
+<button type="submit" name="submitted" value="true">Absenden</button>
 
 </form>
 
-<?php include __DIR__."/php/templates/footer.php" ?>
+<?php }else{ ?>
+    <h3>Vielen Dank für ihre Nachricht <?php echo $_POST["kontakt_name"]?>!</h3>   
+    <p>Wir setzen uns schnellstmöglich mit Ihnen in Verbindung.</p> 
+<?php } ?>
+
+<?php include __DIR__."/templates/footer.php" ?>
 </body>
 </html>
 
